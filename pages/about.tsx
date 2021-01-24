5116cff310eefa263fcb5aff2f49ee36ec09dbd7
FLT4613 <ft4613st@gmail.com>
2021-01-24 12:02:31 +0900

    Fix typeerror

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,11 +4,11 @@ const About = () => (
     <div className="flex justify-center">
         <div>
             <div className="text-center text-4xl">About</div>
-            <Image width="256" height="256" className="rounded-full" src={process.env.NEXT_PUBLIC_PROFILE_SRC} />
+            <Image width="256" height="256" className="rounded-full" src={process.env.NEXT_PUBLIC_PROFILE_SRC || ""} />
             <div className="text-center text-xl">FLT</div>
             <div className="flex justify-evenly">
-                <a href={process.env.NEXT_PUBLIC_GITHUB_URL} target="_blank" rel="noopener noreferer"><Image width="32" height="32" src={process.env.NEXT_PUBLIC_GITHUB_LOGO} /></a>
-                <a href={process.env.NEXT_PUBLIC_TWITTER_URL} target="_blank" rel="noopener noreferer"><Image width="32" height="32" src={process.env.NEXT_PUBLIC_TWITTER_LOGO} /></a>
+                <a href={process.env.NEXT_PUBLIC_GITHUB_URL} target="_blank" rel="noopener noreferer"><Image width="32" height="32" src={process.env.NEXT_PUBLIC_GITHUB_LOGO || ""} /></a>
+                <a href={process.env.NEXT_PUBLIC_TWITTER_URL} target="_blank" rel="noopener noreferer"><Image width="32" height="32" src={process.env.NEXT_PUBLIC_TWITTER_LOGO || ""} /></a>
             </div>
         </div>
     </div>
