d6a193cf0533f7a7987474be508ca727c62aea70
FLT4613 <ft4613st@gmail.com>
2021-02-01 22:27:29 +0900

    Add filtering with input

--- components/post/list.tsx
+++ components/post/list.tsx
@@ -1,14 +1,29 @@
+import { useState, useCallback, useRef } from 'react'
 import { ContentList, Post } from '../../types/types'
 import ListItem from './listitem'
 
-const Component = (props: ContentList<Post>) => (
-    <div className="flex flex-wrap">
-        {props.contents.map((v, i) => (
-            <a href={`/blog/${v.id}`} className="m-1">
-                <ListItem key={i} {...v} />
-            </a>
-        ))}
-    </div>
-)
+const Component = (props: ContentList<Post>) => {
+    const [query, setQuery] = useState("")
+    const handler = useRef<NodeJS.Timeout>()
+    const onInput = useCallback(e => {
+        handler.current && clearTimeout(handler.current)
+        handler.current = setTimeout(() => {
+            setQuery(e.target.value)
+        }, 500)
+    }, [query])
+
+    return (
+        <div>
+            <input className="border my-10 p-2 w-full" type="text" onKeyDown={onInput}></input>
+            <div className="flex flex-wrap">
+                {props.contents.filter(v => !query || (v.title.includes(query) || v.body.includes(query))).map((v, i) => (
+                    <a href={`/blog/${v.id}`} className="m-1">
+                        <ListItem key={i} {...v} />
+                    </a>
+                ))}
+            </div>
+        </div>
+    )
+}
 
 export default Component
