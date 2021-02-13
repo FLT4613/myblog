546863f2fde562ab52b6f65a80897ed2494ffcac
FLT4613 <ft4613st@gmail.com>
2021-02-14 07:42:53 +0900

    improve layout

--- components/post/detail.tsx
+++ components/post/detail.tsx
@@ -2,12 +2,12 @@ import { format, parseISO } from 'date-fns'
 import { Post } from '../../types/types'
 
 const Component = (props: Post) => (
-    <div className="flex flex-col p-5 shadow-md rounded-md overflow-hidden bg-white" key={props.title}>
+    <div className="flex flex-col px-5 pt-8 pb-16 shadow-md rounded-md overflow-hidden bg-white" key={props.title}>
         <div className="text-l">
             {format(parseISO(props.publishedAt), 'yyyy/MM/dd')}
         </div>
         <div className="text-3xl py-3">{props.title}</div>
-        <div className="prose" dangerouslySetInnerHTML={{ __html: props.body }}></div>
+        <div className="prose prose-sm max-w-none" dangerouslySetInnerHTML={{ __html: props.body }}></div>
     </div>
 )
 
